c7b244b8171d744a6e6da8bc06d3b21260edc6ab
test-git <dholariyarahul@gmail.com>
2021-07-01 18:26:42 +0530

    Header Component added to App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header'
 
 function App() {
+  const name='India'
+  let x=false 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Header / >
+        <h1>Jai Bharat</h1>
+        <h2>Namste {name} </h2>
+        <h3>X is:{x ? "Yes": "No" } </h3>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+
+const header = () => {
+    return (
+        <header>
+            <h1>Task Tracker</h1>
+        </header>
+    )
+}
+
+export default header
